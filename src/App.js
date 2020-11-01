b96e83eb1306b80262aef4e255fa5d84d0f4b6bb
PalkaAnshwika <53702328+PalkaAnshwika@users.noreply.github.com>
2020-11-01 18:40:47 +0530

    Latest changes added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import LandingPage from './spaceX/landing-page';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
